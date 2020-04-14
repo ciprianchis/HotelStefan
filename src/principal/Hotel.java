9e2bd38c7770caa04dd282ef5fe9941d9c9719a5
Alex Noe <alejandronoe2610@iesandorra.es>
2020-04-14 18:50:35 +0200

    Problema jlist

--- src/principal/Hotel.java
+++ src/principal/Hotel.java
@@ -17,6 +17,7 @@ import javax.swing.JMenuBar;
 import java.awt.Color;
 import javax.swing.border.BevelBorder;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.border.SoftBevelBorder;
 import java.awt.Font;
@@ -25,6 +26,14 @@ import java.awt.SystemColor;
 import java.awt.Cursor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
 
 public class Hotel extends JFrame {
 
@@ -80,10 +89,12 @@ public class Hotel extends JFrame {
 		contentPane.setLayout(null);
 		
 		btnAsignar = new JButton("ASIGNAR HABITACI\u00D3N");
+		btnAsignar.addMouseListener(new BtnAsignarMouseListener());
 		btnAsignar.setBounds(597, 690, 152, 23);
 		contentPane.add(btnAsignar);
 		
 		lblFinal = new JLabel("Fecha final:");
+		lblFinal.setForeground(Color.WHITE);
 		lblFinal.setBounds(835, 219, 67, 14);
 		contentPane.add(lblFinal);
 		
@@ -92,55 +103,56 @@ public class Hotel extends JFrame {
 		contentPane.add(list);
 		
 		lblInicio = new JLabel("Fecha inicio:");
+		lblInicio.setForeground(Color.WHITE);
 		lblInicio.setBounds(835, 167, 81, 14);
 		contentPane.add(lblInicio);
 		
 		lblReserva = new JLabel("Reserva:");
+		lblReserva.setForeground(Color.WHITE);
 		lblReserva.setBounds(358, 163, 81, 22);
 		contentPane.add(lblReserva);
 		
 		lblNombre = new JLabel("Nombre: ");
+		lblNombre.setForeground(Color.WHITE);
 		lblNombre.setBounds(358, 219, 46, 14);
 		contentPane.add(lblNombre);
 		
 		btnBorrarBusqueda = new JButton("BORRAR BUSQUEDA");
+		btnBorrarBusqueda.addMouseListener(new BtnBorrarBusquedaMouseListener());
 		btnBorrarBusqueda.setBounds(696, 356, 205, 23);
 		contentPane.add(btnBorrarBusqueda);
 		
 		txtNombre = new JTextField();
-		txtNombre.setText("NOMBRE");
 		txtNombre.setColumns(10);
 		txtNombre.setBounds(430, 216, 86, 20);
 		contentPane.add(txtNombre);
 		
 		txtApellidos = new JTextField();
-		txtApellidos.setText("APELLIDOS");
 		txtApellidos.setColumns(10);
 		txtApellidos.setBounds(430, 279, 86, 20);
 		contentPane.add(txtApellidos);
 		
 		txtCodigo = new JTextField();
-		txtCodigo.setText("CODIGO");
 		txtCodigo.setBounds(430, 164, 86, 20);
 		contentPane.add(txtCodigo);
 		txtCodigo.setColumns(10);
 		
 		btnBuscar = new JButton("BUSCAR");
+		btnBuscar.addMouseListener(new BtnBuscarMouseListener());
 		btnBuscar.setBounds(513, 356, 89, 23);
 		contentPane.add(btnBuscar);
 		
 		txtInicio = new JTextField();
-		txtInicio.setText("INICIO");
 		txtInicio.setColumns(10);
 		txtInicio.setBounds(918, 164, 86, 20);
 		contentPane.add(txtInicio);
 		
 		lblApellidos = new JLabel("Apellidos:");
+		lblApellidos.setForeground(Color.WHITE);
 		lblApellidos.setBounds(358, 282, 46, 14);
 		contentPane.add(lblApellidos);
 		
 		txtFinal = new JTextField();
-		txtFinal.setText("FINAL");
 		txtFinal.setColumns(10);
 		txtFinal.setBounds(918, 216, 86, 20);
 		contentPane.add(txtFinal);
@@ -224,6 +236,63 @@ public class Hotel extends JFrame {
 		@Override
 		public void mouseExited(MouseEvent e) {
 			lblMaximizar.setIcon(new ImageIcon("C:\\Users\\Alex Noe\\Desktop\\GRADO SUPERIOR\\PROGRAMACION\\HotelStefan\\recursos\\maximize.png"));
+		}
+	}
+	private class BtnBorrarBusquedaMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			 txtCodigo.setText("CODIGO");
+			 txtNombre.setText("NOMBRE");;
+			 txtApellidos.setText("APELLIDOS");;
+			 txtInicio.setText("INICIO");;
+			 txtFinal.setText("FINAL");;
+			 
+			 JOptionPane.showMessageDialog(null, "Busqueda borrada");
+		}
+	}
+	private class BtnAsignarMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			JOptionPane.showMessageDialog(null, "Habitacion asignada");
+		}
+	}
+	private class BtnBuscarMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			String[] vLista = new String[100];
+			File fichero = new File("reservas.dat");
+			if(!fichero.exists()) {
+				try {
+					fichero.createNewFile();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			
+			else {
+				try {
+					FileInputStream fi = new FileInputStream(fichero);
+		            InputStreamReader is = new InputStreamReader(fi, "UTF8");
+		            BufferedReader br = new BufferedReader(is);
+		            
+					String linea = br.readLine();
+					
+					while(linea!=null) {
+						
+					}
+					
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedEncodingException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} 
+			}
 		}
 	}
 }
