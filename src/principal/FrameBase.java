b82c8df844e984b0f4f327c887d5e321c0494e7d
Borja <borlojaren@gmail.com>
2020-04-14 19:06:33 +0200

    Corrección de estructura

---
+++ src/principal/FrameBase.java
@@ -0,0 +1,148 @@
+package principal;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Rectangle;
+import java.awt.Frame;
+import javax.swing.JMenuBar;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+import javax.swing.border.SoftBevelBorder;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import java.awt.Cursor;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Component;
+
+public class FrameBase extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel lblNewLabel;
+	private JPanel panel;
+	private JLabel lblNombreApp;
+	private JLabel lblCerrar;
+	private JLabel lblMinimizar;
+	private JLabel lblMaximizar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameBase frame = new FrameBase();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameBase() {
+		setResizable(false);
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 750);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(".\\recursos\\fondo.jpg"));
+		lblNewLabel.setBounds(0, 30, 1280, 720);
+		contentPane.add(lblNewLabel);
+		
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBorder(new SoftBevelBorder(BevelBorder.RAISED, Color.LIGHT_GRAY, null, null, null));
+		panel.setBounds(0, 0, 1280, 30);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		lblNombreApp = new JLabel("Hotel Stefan *****");
+		lblNombreApp.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNombreApp.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNombreApp.setFont(new Font("Perpetua Titling MT", Font.BOLD, 18));
+		lblNombreApp.setForeground(new Color(184, 134, 11));
+		lblNombreApp.setBounds(538, 0, 212, 30);
+		panel.add(lblNombreApp);
+		
+		lblCerrar = new JLabel("");
+		lblCerrar.addMouseListener(new LblCerrarMouseListener());
+		lblCerrar.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCerrar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblCerrar.setToolTipText("Cerrar");
+		lblCerrar.setIcon(new ImageIcon(".\\recursos\\close.png"));
+		lblCerrar.setBounds(22, 9, 14, 14);
+		panel.add(lblCerrar);
+		
+		lblMinimizar = new JLabel("");
+		lblMinimizar.addMouseListener(new LblMinimizarMouseListener());
+		lblMinimizar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblMinimizar.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMinimizar.setToolTipText("Minimizar");
+		lblMinimizar.setIcon(new ImageIcon(".\\recursos\\minimize.png"));
+		lblMinimizar.setBounds(46, 9, 14, 14);
+		panel.add(lblMinimizar);
+		
+		lblMaximizar = new JLabel("");
+		lblMaximizar.addMouseListener(new LblMaximizarMouseListener());
+		lblMaximizar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblMaximizar.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMaximizar.setToolTipText("Maximizar");
+		lblMaximizar.setIcon(new ImageIcon(".\\recursos\\maximize.png"));
+		lblMaximizar.setBounds(70, 9, 14, 14);
+		panel.add(lblMaximizar);
+	}
+	private class LblCerrarMouseListener extends MouseAdapter {
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			lblCerrar.setIcon(new ImageIcon(".\\recursos\\closeSelected.png"));
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			lblCerrar.setIcon(new ImageIcon(".\\recursos\\close.png"));
+		}
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			System.exit(0);
+		}
+	}
+	private class LblMinimizarMouseListener extends MouseAdapter {
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			lblMinimizar.setIcon(new ImageIcon(".\\recursos\\minimizeSelected.png"));
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			lblMinimizar.setIcon(new ImageIcon(".\\recursos\\minimize.png"));
+		}
+	}
+	private class LblMaximizarMouseListener extends MouseAdapter {
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			lblMaximizar.setIcon(new ImageIcon(".\\recursos\\maximizeSelected.png"));
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			lblMaximizar.setIcon(new ImageIcon(".\\recursos\\maximize.png"));
+		}
+	}
+}
