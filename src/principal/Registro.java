94c2fbf9602bd6f4eb255382b4c36ff47465259a
Borja <borlojaren@gmail.com>
2020-04-28 00:44:14 +0200

    Implementados recursos gráficos en Registro

--- src/principal/Registro.java
+++ src/principal/Registro.java
@@ -57,6 +57,7 @@ public class Registro extends JFrame {
 	private JButton btnVerContraseña;
 	private boolean mostrar = false;
 	private JPasswordField passwordFieldOculto;
+
 	/**
 	 * Launch the application.
 	 */
@@ -77,8 +78,7 @@ public class Registro extends JFrame {
 	 * Create the frame.
 	 */
 	public Registro() {
-		
-		
+
 		setResizable(false);
 		setUndecorated(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -87,14 +87,14 @@ public class Registro extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		panel.setBorder(new SoftBevelBorder(BevelBorder.RAISED, Color.LIGHT_GRAY, null, null, null));
 		panel.setBounds(0, 0, 1280, 30);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		lblNombreApp = new JLabel("Hotel Stefan *****");
 		lblNombreApp.setBounds(538, 0, 212, 30);
 		lblNombreApp.setVerticalAlignment(SwingConstants.BOTTOM);
@@ -102,7 +102,7 @@ public class Registro extends JFrame {
 		lblNombreApp.setFont(new Font("Perpetua Titling MT", Font.BOLD, 18));
 		lblNombreApp.setForeground(new Color(184, 134, 11));
 		panel.add(lblNombreApp);
-		
+
 		lblCerrar = new JLabel("");
 		lblCerrar.setBounds(22, 9, 14, 14);
 		lblCerrar.addMouseListener(new LblCerrarMouseListener());
@@ -111,7 +111,7 @@ public class Registro extends JFrame {
 		lblCerrar.setToolTipText("Cerrar");
 		lblCerrar.setIcon(new ImageIcon(".\\recursos\\close.png"));
 		panel.add(lblCerrar);
-		
+
 		lblMinimizar = new JLabel("");
 		lblMinimizar.setBounds(46, 9, 14, 14);
 		lblMinimizar.addMouseListener(new LblMinimizarMouseListener());
@@ -120,7 +120,7 @@ public class Registro extends JFrame {
 		lblMinimizar.setToolTipText("Minimizar");
 		lblMinimizar.setIcon(new ImageIcon(".\\recursos\\minimize.png"));
 		panel.add(lblMinimizar);
-		
+
 		lblMaximizar = new JLabel("");
 		lblMaximizar.setBounds(70, 9, 14, 14);
 		lblMaximizar.addMouseListener(new LblMaximizarMouseListener());
@@ -129,7 +129,7 @@ public class Registro extends JFrame {
 		lblMaximizar.setToolTipText("Maximizar");
 		lblMaximizar.setIcon(new ImageIcon(".\\recursos\\maximize.png"));
 		panel.add(lblMaximizar);
-		
+
 		textFieldUser = new JTextField();
 		textFieldUser.addFocusListener(new TextFieldUserFocusListener());
 		textFieldUser.setText("USUARIO");
@@ -138,7 +138,7 @@ public class Registro extends JFrame {
 		textFieldUser.setBounds(457, 200, 368, 40);
 		contentPane.add(textFieldUser);
 		textFieldUser.setColumns(10);
-		
+
 		lblRegistro = new JLabel("REGISTRO");
 		lblRegistro.setFocusable(false);
 		lblRegistro.setForeground(new Color(184, 134, 11));
@@ -146,23 +146,31 @@ public class Registro extends JFrame {
 		lblRegistro.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRegistro.setBounds(457, 62, 368, 70);
 		contentPane.add(lblRegistro);
-		
+
 		btnAñadirUser = new JLabel("");
+		btnAñadirUser.setToolTipText("A\u00F1adir usuario");
+		btnAñadirUser.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnAñadirUser.setIcon(new ImageIcon("C:\\Users\\34658\\gitKraken\\HotelStefan\\HotelStefan\\recursos\\addUserBW.png"));
+		btnAñadirUser.addMouseListener(new BtnAñadirUserMouseListener());
 		btnAñadirUser.setHorizontalAlignment(SwingConstants.CENTER);
 		btnAñadirUser.setFocusable(false);
 		btnAñadirUser.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnAñadirUser.setBounds(250, 550, 128, 128);
+		btnAñadirUser.setBounds(406, 550, 128, 128);
 		contentPane.add(btnAñadirUser);
-		
+
 		btnSignIn = new JLabel("");
+		btnSignIn.setToolTipText("Volver a Login");
+		btnSignIn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnSignIn.setIcon(
+				new ImageIcon("C:\\Users\\34658\\gitKraken\\HotelStefan\\HotelStefan\\recursos\\goBackBW.png"));
 		btnSignIn.setHorizontalAlignment(SwingConstants.CENTER);
 		btnSignIn.setFocusable(false);
 		btnSignIn.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnSignIn.setBounds(660, 550, 128, 128);
+		btnSignIn.setBounds(751, 550, 128, 128);
 		btnSignIn.addMouseListener(new BtnSignInMouseListener());
-		
+
 		contentPane.add(btnSignIn);
-		
+
 		txtContraseaVisible = new JTextField();
 		txtContraseaVisible.addFocusListener(new TxtContraseaFocusListener());
 		txtContraseaVisible.setFont(new Font("Monospaced", Font.PLAIN, 20));
@@ -171,7 +179,7 @@ public class Registro extends JFrame {
 		txtContraseaVisible.setBounds(457, 275, 368, 40);
 		contentPane.add(txtContraseaVisible);
 		txtContraseaVisible.setColumns(10);
-		
+
 		chckbxAdmin = new JCheckBox(" ADMINISTRADOR");
 		chckbxAdmin.setForeground(SystemColor.window);
 		chckbxAdmin.setHorizontalAlignment(SwingConstants.CENTER);
@@ -179,17 +187,17 @@ public class Registro extends JFrame {
 		chckbxAdmin.setFont(new Font("Monospaced", Font.PLAIN, 20));
 		chckbxAdmin.setBounds(457, 400, 368, 40);
 		contentPane.add(chckbxAdmin);
-		
+
 		btnVerContraseña = new JButton("--");
 		btnVerContraseña.addActionListener(new BtnVerContraseñaActionListener());
 		btnVerContraseña.setBounds(835, 287, 89, 23);
 		contentPane.add(btnVerContraseña);
-		
+
 		passwordFieldOculto = new JPasswordField();
 		passwordFieldOculto.setHorizontalAlignment(SwingConstants.CENTER);
 		passwordFieldOculto.setBounds(457, 275, 368, 40);
 		contentPane.add(passwordFieldOculto);
-		
+
 		lblFondo = new JLabel("");
 		lblFondo.setIcon(new ImageIcon(".\\recursos\\fondo.jpg"));
 		lblFondo.setBounds(0, 30, 1280, 720);
@@ -197,49 +205,57 @@ public class Registro extends JFrame {
 		lblFondo.setFocusable(true);
 		lblFondo.requestFocus();
 		vUsuarios = IoDatos.leerDatos();
-		
+
 		txtContraseaVisible.setVisible(false);
 		passwordFieldOculto.setVisible(true);
-		
-		
+
 	}
+
 	private class LblCerrarMouseListener extends MouseAdapter {
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			lblCerrar.setIcon(new ImageIcon(".\\recursos\\closeSelected.png"));
 		}
+
 		@Override
 		public void mouseExited(MouseEvent e) {
 			lblCerrar.setIcon(new ImageIcon(".\\recursos\\close.png"));
 		}
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			System.exit(0);
 		}
 	}
+
 	private class LblMinimizarMouseListener extends MouseAdapter {
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			lblMinimizar.setIcon(new ImageIcon(".\\recursos\\minimizeSelected.png"));
 		}
+
 		@Override
 		public void mouseExited(MouseEvent e) {
 			lblMinimizar.setIcon(new ImageIcon(".\\recursos\\minimize.png"));
 		}
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			setState(JFrame.ICONIFIED);
 		}
 	}
+
 	private class LblMaximizarMouseListener extends MouseAdapter {
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			lblMaximizar.setIcon(new ImageIcon(".\\recursos\\maximizeSelected.png"));
 		}
+
 		@Override
 		public void mouseExited(MouseEvent e) {
 			lblMaximizar.setIcon(new ImageIcon(".\\recursos\\maximize.png"));
 		}
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			if (!maximizado) {
@@ -252,15 +268,17 @@ public class Registro extends JFrame {
 
 		}
 	}
+
 	private class TextFieldUserFocusListener extends FocusAdapter {
 		@Override
 		public void focusGained(FocusEvent arg0) {
 			if (textFieldUser.getText().equals("USUARIO")) {
 				textFieldUser.setText("");
 			}
-			
+
 			lblFondo.setFocusable(false);
 		}
+
 		@Override
 		public void focusLost(FocusEvent arg0) {
 			if (textFieldUser.getText().equals("")) {
@@ -268,6 +286,7 @@ public class Registro extends JFrame {
 			}
 		}
 	}
+
 	private class BtnSignInMouseListener extends MouseAdapter {
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
@@ -275,43 +294,71 @@ public class Registro extends JFrame {
 			log.setVisible(true);
 			dispose();
 		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			btnSignIn.setIcon(new ImageIcon(".\\recursos\\goBack.png"));
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			btnSignIn.setIcon(new ImageIcon(".\\recursos\\goBackBW.png"));
+		}
 	}
-	
+
 	private class TxtContraseaFocusListener extends FocusAdapter {
 		@Override
 		public void focusGained(FocusEvent e) {
 			txtContraseaVisible.setText("");
-		}	
+		}
+
 		@Override
 		public void focusLost(FocusEvent arg0) {
 			txtContraseaVisible.setText(txtContraseaVisible.getText());
-			
+
 			Pattern pPasswd = Pattern.compile("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$");
 			Matcher mPasswd = pPasswd.matcher(txtContraseaVisible.getText());
-			
+
 			if (txtContraseaVisible.getText().equals("")) {
 				txtContraseaVisible.setText("CONTRASEÑA");
 			}
-			
+
 			if (!mPasswd.matches()) {
-				JOptionPane.showMessageDialog(null, "La contraseña debe contener un mínimo de 8 carácteres y máximo 16."+ "\n"
-						+ "Mayúsculas, minúsculas y números, uno de cada tipo por lo menos.");
+				JOptionPane.showMessageDialog(null, "La contraseña debe contener un mínimo de 8 carácteres y máximo 16."
+						+ "\n" + "Mayúsculas, minúsculas y números, uno de cada tipo por lo menos.");
 			}
-			
-			
+
 		}
 	}
-	private class BtnAñadirUserActionListener implements ActionListener {
+
+	private class BtnVerContraseñaActionListener implements ActionListener {
 		public void actionPerformed(ActionEvent arg0) {
-			// ERROR CONTRASEÑA					
+			if (mostrar) {
+				txtContraseaVisible.setVisible(true);
+				passwordFieldOculto.setVisible(false);
+				txtContraseaVisible.setText(String.valueOf(passwordFieldOculto.getPassword()));
+				mostrar = false;
+			} else {
+				txtContraseaVisible.setVisible(false);
+				passwordFieldOculto.setVisible(true);
+				passwordFieldOculto.setText(txtContraseaVisible.getText());
+				mostrar = true;
+			}
+		}
+	}
+
+	private class BtnAñadirUserMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// ERROR CONTRASEÑA
 			Pattern pPasswd = Pattern.compile("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$");
 			Matcher mPasswd = pPasswd.matcher(txtContraseaVisible.getText());
-			
+
 			if (!mPasswd.matches()) {
 				JOptionPane.showMessageDialog(null, "La contraseña no cumple los requisitos");
 				return;
 			}
-			//FIN CORRECCIÓN ERROR CONTRASEÑA 
+			// FIN CORRECCIÓN ERROR CONTRASEÑA
 			for (Usuario usuario : vUsuarios) {
 				if (usuario.getNombreUsuario().equals(textFieldUser.getText())) {
 					JOptionPane.showMessageDialog(null, "El usuario indicado ya fue introducido anteriormente");
@@ -321,36 +368,24 @@ public class Registro extends JFrame {
 					return;
 				}
 			}
-			
-			
+
 			JOptionPane.showMessageDialog(null, "El nuevo usuario fue introducido");
-			
-			
+
 			Usuario usu = new Usuario(textFieldUser.getText(), txtContraseaVisible.getText(), chckbxAdmin.isSelected());
 			vUsuarios.add(usu);
-			
+
 			IoDatos.guardarUsusarios(vUsuarios);
 			textFieldUser.setText("USUARIO");
 			txtContraseaVisible.setText("CONTRASEÑA");
 			chckbxAdmin.setSelected(false);
-			
-			
-			
 		}
-	}
-	private class BtnVerContraseñaActionListener implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			if (mostrar) {
-				txtContraseaVisible.setVisible(true);
-				passwordFieldOculto.setVisible(false);
-				txtContraseaVisible.setText(String.valueOf(passwordFieldOculto.getPassword()));
-				mostrar = false;
-			}else {
-				txtContraseaVisible.setVisible(false);
-				passwordFieldOculto.setVisible(true);
-				passwordFieldOculto.setText(txtContraseaVisible.getText());
-				mostrar = true;
-			}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			btnAñadirUser.setIcon(new ImageIcon(".\\recursos\\addUser.png"));
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			btnAñadirUser.setIcon(new ImageIcon(".\\recursos\\addUserBW.png"));
 		}
 	}
 }
